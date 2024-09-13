7d92bc25d32108520ce6e44be8115d1b88f2c8a1
Thync <thync@outlook.com>
2024-09-13 16:49:39 +0700

    Update: flow

---
+++ vbroker/__init__.py
@@ -0,0 +1,4 @@
+""" Broker module. """
+from .enum_broker import EnumBroker  # noqa: F401
+from .broker import Broker  # noqa: F401
+from .config import Config  # noqa: F401
--- vbroker/ssi/api.py
+++ vbroker/ssi/api.py
@@ -4,3 +4,6 @@ from ..interface_broker_api import IBrokerAPI
 class SSIBrokerAPI(IBrokerAPI):
     def __init__(self, config):
         self.config = config
+
+    def get_token(self):
+        return "token"
